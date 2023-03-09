027aa33b445a4bf8f8f3c79d4b1109aa76420a0f
John B <100374955+jtbrennan@users.noreply.github.com>
2023-03-09 09:44:27 -0500

    Add files via upload

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,42 @@
+const Course = ({ course }) => {
+    return (
+      <div>
+        <Header name={course.name} />
+        <Content parts={course.parts} />
+        <Total parts={course.parts} />
+      </div>
+    );
+  };
+
+  const Part = ({ name, exercises }) => {
+    return (
+      <p>{name}{exercises}</p>
+    );
+  };
+
+  const Header = ({ name }) => {
+    return <h2>{name}</h2>;
+  };
+  
+
+
+
+
+
+  const Content = ({ parts }) => {
+    return (
+      <div>{parts.map((part) => (
+          <Part key={part.id} name={part.name} exercises={part.exercises} />
+        ))}</div>
+    );
+  };
+    
+  const Total = ({ parts }) => {
+    const total = parts.reduce((sum, part) => {
+      return sum + part.exercises;
+    }, 0);
+  
+    return <strong>total of {total} exercises</strong>;
+  };
+  
+  export default Course;
